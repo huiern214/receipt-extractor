7096e9396d8812812716c3f42ac3d0731bd565fc
Hui Ern <huiern214@gmail.com>
2023-03-22 04:14:44 +0800

    Modified

--- receipt-extractor.py
+++ receipt-extractor.py
@@ -14,13 +14,11 @@ import webbrowser
 from googleapiclient import discovery, http
 from httplib2 import Http
 from oauth2client import file, client, tools
-from google.cloud import vision
 from google.cloud import storage
 
-BUCKET = 'cloud-workshop-bucket' # bucket name
-PARENT = 'receipts'     # YOUR IMG FILE PREFIX  
-SHEET = '1sdwPpYT8dscUKPSirkBQSlEvKe-oZVAlyAXDF3w7P_w' # sheets id
-TOP = 5       # TOP # of LABELS TO SAVE
+BUCKET = 'cloud-workshop-bucket' # BUCKET NAME
+PARENT = 'receipts'     # YOUR IMG FILE PREFIX / FOLDER IN BUCKET 
+SHEET = '1sdwPpYT8dscUKPSirkBQSlEvKe-oZVAlyAXDF3w7P_w' # SHEETS ID
 DEBUG = False
 
 # process credentials for OAuth2 tokens
@@ -44,11 +42,10 @@ GCS    = discovery.build('storage', 'v1', http=HTTP)
 VISION = discovery.build('vision',  'v1', http=HTTP)
 SHEETS = discovery.build('sheets',  'v4', http=HTTP)
 
-
 def drive_get_img():
     'download files from Drive and return file info & binary if found'
     folder_id = '1YOoKlYrI8IKQkU_CwGfpo3jDyyKoX_aP' # search for file on Google Drive in the given directory
-    query = "'%s' in parents" % (folder_id)
+    query = "'%s' in parents and trashed=false" % (folder_id) # list all files in the drive folder
     rsp = DRIVE.files().list(q=query, fields='files(id,name,mimeType,modifiedTime)').execute().get('files', [])
     
     # Return file info if found, else return None
@@ -66,16 +63,15 @@ def gcs_blob_upload(fname, bucket, media, mimetype):
             fields='bucket,name').execute()
 
 
-def vision_detect_text_img(img, top):
+def vision_detect_text_img(img):
     'send image to Vision API for text annotation'
 
     # build image metadata and call Vision API to process
     body = {'requests': [{
                 'image': {'content': img},
-                'features': [{'type': 'TEXT_DETECTION', 'maxResults': top}],
+                'features': {'type': 'TEXT_DETECTION'},
     }]}
     rsp = VISION.images().annotate(body=body).execute().get('responses', [{}])[0]
-
     def containsCaseInsensitive(substring, string):
         if substring.lower() in string.lower():
             return True
@@ -83,19 +79,21 @@ def vision_detect_text_img(img, top):
             return False
             
     extracted_text, date, total_price = '', '',''
-    # return top labels for image as CSV for Sheet (row)
+    # return extracted text from image and parsed data for Sheet (row)
+    # Since the format of receipts may vary, the text parsing requires constant bug fixing and updating.
     if 'textAnnotations' in rsp:
         extracted_text = rsp.get('textAnnotations', [{}])[0].get('description', '')
+        # print(extracted_text)
         lines = extracted_text.split("\n")
-        shop_name = lines[0]
-        shop_address = lines[1] + " " + lines[2] + " " + lines[3]
+        shop_name = lines[0] #shope name
+        shop_address = lines[1] + " " + lines[2] + " " + lines[3] # shop address / location
 
         for line in lines:
             if containsCaseInsensitive("Date", line):
-                date = line.split(" ")[1]
+                date = line.split(" ")[1] # purchase date
             if containsCaseInsensitive("Total", line):
                 if containsCaseInsensitive("RM", line):
-                    total_price = line.split("RM")[-1]
+                    total_price = line.split("RM")[-1] # total price
                 else: 
                     total_price = line.split(" ")[-1]
     
@@ -114,33 +112,35 @@ def sheet_append_row(sheet, row):
         return rsp.get('updates').get('updatedCells')
 
 
-def main(bucket, sheet_id, folder, top, debug):
+def main(bucket, sheet_id, folder, debug):
     '"main()" drives process from image download through report generation'
 
     # download img file & info from Drive
-    rsp = drive_get_img()
+    rsp = drive_get_img() # get a list of files from Drive
     if not rsp:
         return
     if debug:
         print(rsp)
+        print()
 
-    for target in rsp:
+    # process the img files one by one using loop
+    for target in rsp: 
         fileId = target['id']
         fname = target['name']
         mtype = target['mimeType']
-        data = DRIVE.files().get_media(fileId=fileId).execute() #binary data
+        data = DRIVE.files().get_media(fileId=fileId).execute() # binary data
         ftime = target['modifiedTime']
 
         # Create a client object
         ## for local run, https://cloud.google.com/docs/authentication/client-libraries
-        ## client = storage.Client(project="cloud-workshop-380401")
+        # client = storage.Client(project="cloud-workshop-380401")
+        ## for remote run in the Cloud Shell Editor,
         client = storage.Client()
-        # Define the prefix to search for the file in the my_folder directory
-        prefix = folder + '/'
-        # List all the blobs in the bucket with the specified prefix
-        blobs = client.get_bucket(bucket).list_blobs(prefix=prefix)
+        # List all the files (blobs) in the bucket with the specified prefix (folder/directory)
+        blobs = client.get_bucket(bucket).list_blobs(prefix=folder)
         files=[a.name for a in blobs]
-        if (prefix + fname) in files:
+        # Check if the filename of the image in Google Drive exists in the Google Cloud Storage bucket.
+        if (folder + '/' + fname) in files:
             continue
 
         # upload file to GCS
@@ -149,14 +149,14 @@ def main(bucket, sheet_id, folder, top, debug):
         if not rsp:
             return
         if debug:
-            print('Uploaded %r to GCS bucket %r' % (rsp['name'], rsp['bucket']))
+            print('Uploaded %r to GCS bucket %r\n' % (rsp['name'], rsp['bucket']))
 
         # process w/Vision
-        rsp, extracted_text, shop_name, shop_address, date, total_price = vision_detect_text_img(base64.b64encode(data).decode('utf-8'), top)
+        rsp, extracted_text, shop_name, shop_address, date, total_price = vision_detect_text_img(base64.b64encode(data).decode('utf-8'))
         if not rsp:
             return
         if debug:
-            print('Top %d responses from Vision API: %s' % (top, rsp))
+            print('Extracted text from Vision API response:\n %s\n' % (extracted_text))
 
         # push results to Sheet, get cells-saved count
         row = [date,
@@ -167,30 +167,25 @@ def main(bucket, sheet_id, folder, top, debug):
         if not rsp:
             return
         if debug:
-            print('Added %d cells to Google Sheet' % rsp)
+            print('Added %d cells to Google Sheet\n' % rsp)
     return True
 
 
 if __name__ == '__main__':
-    # args: [-hv] [-i imgfile] [-b bucket] [-f folder] [-s Sheet ID] [-t top labels]
+    # args: [-hv] [-b bucket] [-f folder] [-s Sheet ID]
     parser = argparse.ArgumentParser()
-    # parser.add_argument("-i", "--imgfile", action="store_true",
-    #         default=FILE, help="image file filename")
     parser.add_argument("-b", "--bucket_id", action="store_true",
             default=BUCKET, help="Google Cloud Storage bucket name")
     parser.add_argument("-f", "--folder", action="store_true",
             default=PARENT, help="Google Cloud Storage image folder")
     parser.add_argument("-s", "--sheet_id", action="store_true",
             default=SHEET, help="Google Sheet Drive file ID (44-char str)")
-    parser.add_argument("-t", "--viz_top", action="store_true",
-            default=TOP, help="return top N (default %d) Vision API labels" % TOP)
     parser.add_argument("-v", "--verbose", action="store_true",
             default=DEBUG, help="verbose display output")
     args = parser.parse_args()
 
     print('Processing files... please wait')
-    rsp = main(args.bucket_id,
-            args.sheet_id, args.folder, args.viz_top, args.verbose)
+    rsp = main(args.bucket_id, args.sheet_id, args.folder, args.verbose)
     if rsp:
         sheet_url = 'https://docs.google.com/spreadsheets/d/%s/edit' % args.sheet_id
         print('DONE: opening web browser to it, or see %s' % sheet_url)
